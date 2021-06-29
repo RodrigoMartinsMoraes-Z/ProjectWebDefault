055ebfd9446cc0d8a543cd390be8654e270a3fd9
DESKTOP-M8CDOTN\rodri <rodrigo_moraes@hotmail.com.br>
2021-06-29 15:03:48 -0300

    product controller english

--- Project.Web/wwwroot/controllers/productController.js
+++ Project.Web/wwwroot/controllers/productController.js
@@ -3,40 +3,40 @@
 
     app.controller('productController', ['$scope', '$http', '$localStorage', '$window', function (scope, http, local, window) {
 
-        scope.catCadastrada = false;
+        scope.categoryRegistred = false;
         scope.product = {};
         scope.product.images = [];
         scope.product.imagesCount = 0;
         scope.take = 10;
         scope.skip = 0;
 
-        scope.carregarProduto = function () {
+        scope.getProduct = function () {
             http.get("api/product/" + model, local.config)
                 .then(function (r) {
                     scope.product = r.data;
                 })
         };
 
-        scope.novaCategoria = function () {
+        scope.newCategory = function () {
             http.put("/api/category/", scope.newCategory, local.config)
                 .then(function () {
                     scope.newCategory = {};
-                    scope.catCadastrada = true;
+                    scope.categoryRegistred = true;
                 })
                 .catch(function () {
                     alert("Categoria já cadastrada!");
-                    scope.catCadastrada = false;
+                    scope.categoryRegistred = false;
                 });
         };
 
-        scope.buscarCategorias = function () {
+        scope.getCategories = function () {
             http.get("/api/category/", local.config)
                 .then(function (r) {
                     scope.categories = r.data;
                 });
         };
 
-        scope.cadastrarProduto = function () {
+        scope.newProduct = function () {
             http.put("/api/product/update", scope.product, local.config)
                 .then(function () {
 
@@ -46,17 +46,17 @@
                 });
         };
 
-        scope.novoProduto = function () {
+        scope.newProductPage = function () {
             window.location.href = '/produto';
         };
 
-        scope.detalhesProduto = function (id) {
+        scope.productDetailsPage = function (id) {
             window.location.href = '/produto/' + id;
 
         };
 
-        scope.uparImagem = function () {
-            var f = document.getElementById('imagemDoProduto');
+        scope.loadImage = function () {
+            var f = document.getElementById('productImage');
 
             var i = 0;
             for (i = 0; i < f.files.length; i++) {
@@ -82,32 +82,27 @@
 
         };
 
-        scope.deletarImagem = function (id) {
+        scope.deleteImage = function (id) {
             http.delete("api/file/delete-image/" + id, local.config)
                 .then(function () {
                     scope.product.images = scope.product.images.filter(image => image.id != id);
                 });
         };
 
-        scope.teste = function (id) {
-            console.log(id);
-            scope.product.images = scope.product.images.filter(image => image.id != id);
-        };
-
-        scope.listarProdutos = function () {
+        scope.listProducts = function () {
             http.get("api/product/list?take=" + scope.take + "&skip=" + scope.skip)
                 .then(function (r) {
                     scope.products = r.data;
                 });
         }
 
-        scope.imagemAnterior = function (min, imageIndex) {
+        scope.previousImage = function (min, imageIndex) {
             if (imageIndex > min)
                 return imageIndex - 1;
             return imageIndex;+-..
         };
 
-        scope.proximaImagem = function (max, imageIndex) {
+        scope.nextImage = function (max, imageIndex) {
             if (imageIndex < max - 1)
                 return imageIndex + 1;
             return imageIndex;
