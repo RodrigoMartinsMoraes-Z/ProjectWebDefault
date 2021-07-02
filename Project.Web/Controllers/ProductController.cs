ff726a632974aae6369d966a5537739043e80956
DESKTOP-M8CDOTN\rodri <rodrigo_moraes@hotmail.com.br>
2021-07-02 09:38:11 -0300

    swagger ok

--- Project.Web/Controllers/ProductController.cs
+++ Project.Web/Controllers/ProductController.cs
@@ -5,11 +5,13 @@ namespace Project.Web.Controllers
     public class ProductController : Controller
     {   
         [Route("product")]
+        [ApiExplorerSettings(IgnoreApi = true)]
         public IActionResult Index()
         {
             return View(null);
         }        
         [Route("product/{x}")]
+        [ApiExplorerSettings(IgnoreApi = true)]
         public IActionResult Index(int? x = null)
         {
             return View(x);
